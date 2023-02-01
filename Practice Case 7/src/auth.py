8feeaff290437d1194a4a14fab00e8b26d80cbe6
mhmdzakyy <58827040+mhmdzakyy@users.noreply.github.com>
2023-02-01 15:56:14 +0700

    Practice Case 7

---
+++ Practice Case 7/src/auth.py
@@ -0,0 +1,6 @@
+# Credentials (INSERT YOUR KEYS AND TOKENS IN THE STRINGS BELOW)
+api_key = "sE20jJKucWUbCxYZFDhlsEbLw"
+api_secret = "3H6kMVGaQkXxQt3g2pDuEwffaeo54qjvARxs8ZNBoRyIxX9b57"
+bearer_token = r"AAAAAAAAAAAAAAAAAAAAAMlPZgEAAAAAXzorxBVj7NnzR2%2BTgI9o8%2FkxyMc%3D6LPXME9bTpU3fvy4sSFteBs6iZBOVI01ttqiCDF8SWgba3cmca"
+access_token = "1696308313-mo95EYgU1zToVvs0dZOxUOow1Pl4llnbvKmQYVY"
+access_token_secret = "asx75NsADB9Q2AzaxgtC0DbVtiIgte9IGqVcKNuFIV2Ip"
